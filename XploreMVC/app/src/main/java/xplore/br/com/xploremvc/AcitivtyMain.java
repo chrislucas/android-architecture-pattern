2bc962ffec77d51d0f0bbef851871516c3be9fec
chrislucas <christoffer.luccas@gmail.com>
2017-09-12 17:23:45 -0300

    first commit

---
+++ XploreMVC/app/src/main/java/xplore/br/com/xploremvc/AcitivtyMain.java
@@ -0,0 +1,13 @@
+package xplore.br.com.xploremvc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcitivtyMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
